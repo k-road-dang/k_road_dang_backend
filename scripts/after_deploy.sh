24941f0e97125274998914dd9d5eb3b7293ee98e
sangwonyou <tkddnjs231@naver.com>
2022-06-19 16:39:33 +0900

    nvm path 수정

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ec2-user/nest-app
 cd $REPOSITORY
 
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="~/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
