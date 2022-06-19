01e6baa4b344575d1359c35a33670797d2a7af8e
sangwonyou <tkddnjs231@naver.com>
2022-06-19 14:54:59 +0900

    nvm 등록

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -4,14 +4,14 @@ REPOSITORY=/home/ec2-user/nest-app
 cd $REPOSITORY
 
 # nvm 환경변수 등록
-#export NVM_DIR="/home/ec2-user/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-#
-#cd secret
-#sudo mv .env.development ../
-#cd ../
-#rm -rf secret
-#
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+cd secret
+sudo mv .env.development ../
+cd ../
+rm -rf secret
+
 
 yarn start
