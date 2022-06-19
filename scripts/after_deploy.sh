6362798aa79a4d84d12844c1fa554664e0c09bfb
sangwonyou <tkddnjs231@naver.com>
2022-06-19 16:43:04 +0900

    version test

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -7,7 +7,9 @@ export NVM_DIR="~/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-npm install -g yarn
+nvm -v
 
-yarn start:prod
+#npm install -g yarn
+#
+#yarn start:prod
 
