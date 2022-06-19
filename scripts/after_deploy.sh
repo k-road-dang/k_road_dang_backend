9702d23487bef2b67e0e0c5c6ec120b9a64e61ac
sangwonyou <tkddnjs231@naver.com>
2022-06-19 18:26:29 +0900

    check version

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,7 +2,8 @@
 #!/bin/bash
 cd /home/ec2-user/nest-app
 
-export NVM_DIR="/home/ec2-user/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 source /home/ec2-user/.bash_profile
 
 npm install -g yarn
