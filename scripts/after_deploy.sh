e6f28bd90ac5b924d96b3996f464b44a9ba60251
sangwonyou <tkddnjs231@naver.com>
2022-06-19 20:36:04 +0900

    push

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,11 +2,13 @@
 #!/bin/bash
 cd /home/ec2-user/nest-app
 
-export NVM_DIR="/home/ec2-user/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+export NVM_DIR="~/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 source /home/ec2-user/.bash_profile
 
-npm install -g yarn
+#npm install -g yarn
 #
 yarn start:prod
 
