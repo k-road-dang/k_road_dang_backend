0c3396ca264067555f68c4bb7009fc4102263243
sangwonyou <tkddnjs231@naver.com>
2022-06-22 20:25:21 +0900

    주석 테스트

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -6,9 +6,9 @@ export NVM_DIR="~/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-source /home/ec2-user/.bash_profile
+#source /home/ec2-user/.bash_profile
 
 #npm install -g yarn
 #
-yarn start:prod
+#yarn start:prod
 
