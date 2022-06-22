438fff3ca8be455d0c182e190e79a42afc0c1a3e
sangwonyou <tkddnjs231@naver.com>
2022-06-22 20:28:51 +0900

    다 주석

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 cd /home/ec2-user/nest-app
 
-export NVM_DIR="~/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#export NVM_DIR="~/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 #source /home/ec2-user/.bash_profile
 
