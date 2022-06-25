12455d2fe82a3653e802acfc8860fd958aa1ff2f
sangwonyou <tkddnjs231@naver.com>
2022-06-25 12:59:03 +0900

    yarn start

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,16 +2,6 @@
 #!/bin/bash
 cd /home/ec2-user/nest-app
 
-#export NVM_DIR="~/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-
-
-#
-#nvm -v
-#
-#yarn start:prod
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
@@ -27,6 +17,6 @@ nvm alias default 16.15.1
 node -v
 
 npm install --global yarn
-yarn start:prod
+#yarn start:prod
 
 
