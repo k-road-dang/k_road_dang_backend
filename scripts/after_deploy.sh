0567b522e81ee6e72ba91137c001bb6428f34ced
sangwonyou <tkddnjs231@naver.com>
2022-06-25 13:16:06 +0900

    test

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 cd /home/ec2-user/nest-app
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
