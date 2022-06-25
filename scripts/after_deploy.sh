40eac6b48be62f22642e2071ca653d9a9840ce92
sangwonyou <tkddnjs231@naver.com>
2022-06-25 10:38:14 +0900

    npm install

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -8,9 +8,7 @@ cd /home/ec2-user/nest-app
 
 source /home/ec2-user/.bash_profile
 
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads
+
 #
 #nvm -v
 #
@@ -18,6 +16,11 @@ source /home/ec2-user/.bash_profile
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
 source .bash_profile
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads
+
+nvm -v
 whereis node
 
 which npm
