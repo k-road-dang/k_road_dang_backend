86d301252252db01d058a16d30014a9f87e6dde8
sangwonyou <tkddnjs231@naver.com>
2022-06-25 17:26:23 +0900

    start

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -4,11 +4,11 @@ cd /home/ec2-user/nest-app
 
 #curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads
-
-source /home/ec2-user/.bash_profile
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads
+#
+#source /home/ec2-user/.bash_profile
 #sudo pm2 kill
 #
 #nvm install 16.15.1
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,12 @@
 # scripts/after_deploy.sh
 #!/bin/bash
+cd /home/ec2-user/nest-app
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads
+
+source /home/ec2-user/.bash_profile
 
 npm run start:prod
 
