c853a47a3d97461c19bf3475289fc6111d1d2f94
sangwonyou <tkddnjs231@naver.com>
2022-06-25 10:19:21 +0900

    npm 위치

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -8,11 +8,11 @@ cd /home/ec2-user/nest-app
 
 source /home/ec2-user/.bash_profile
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads
-
-nvm -v
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads
+#
+#nvm -v
 #
 #yarn start:prod
 
