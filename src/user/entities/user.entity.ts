4092625df548861b548b8603686dd5cea45f237a
sangwonyou <tkddnjs231@naver.com>
2022-07-03 13:28:50 +0900

    feat: user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -2,16 +2,15 @@ import {
   Column,
   CreateDateColumn,
   Entity,
-  PrimaryColumn,
   PrimaryGeneratedColumn,
   Unique,
   UpdateDateColumn,
 } from 'typeorm';
-import { LoginType } from '../../enum/AppEnum';
+import { Gender, LoginType } from '../../enum/AppEnum';
 import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'user' })
-@Unique(['userId'])
+@Unique(['id'])
 export class User {
   @PrimaryGeneratedColumn('rowid')
   @ApiProperty({
@@ -23,7 +22,7 @@ export class User {
   @ApiProperty({
     description: '사용자 nickname',
   })
-  nickname: string;
+  social_id: string;
 
   @Column({
     nullable: false,
@@ -32,9 +31,57 @@ export class User {
     default: LoginType.Kakao,
   })
   @ApiProperty({
-    description: '사용자 loginType',
+    description: '사용자 loginType Apple || Kakao',
   })
-  loginType: LoginType;
+  login_type: LoginType;
+
+  @Column({ nullable: false })
+  @ApiProperty({
+    description: '사용자 nickname',
+  })
+  nickname: string;
+
+  @Column({ nullable: false })
+  @ApiProperty({
+    description: '사용자 관심 카테고리 ex) 등산, 라이딩',
+  })
+  category: string;
+
+  @Column({ nullable: false, type: 'enum', enum: Gender, default: Gender.M })
+  @ApiProperty({
+    description: '사용자 성별',
+  })
+  gender: Gender;
+
+  @Column({ nullable: false })
+  @ApiProperty({
+    description: '사용자 위도',
+  })
+  lat: number;
+
+  @Column({ nullable: false })
+  @ApiProperty({
+    description: '사용자 경도',
+  })
+  lng: number;
+
+  @Column({ nullable: false })
+  @ApiProperty({
+    description: '프로필 이미지 경로 ( s3 )',
+  })
+  image: string;
+
+  @Column({ nullable: false })
+  @ApiProperty({
+    description: '등급',
+  })
+  grade: string;
+
+  @Column({ nullable: false })
+  @ApiProperty({
+    description: '사용자 경험치',
+  })
+  exp: number;
 
   @CreateDateColumn({ type: 'timestamp' })
   createdAt: Date;
